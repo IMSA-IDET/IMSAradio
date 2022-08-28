0db1724102e334c66966ff63cbc043038bb27796
vellaG <jeremiahsuarez7267@gmail.com>
2022-08-28 15:15:30 -0500

    asd

---
+++ public/js/announcements.js
@@ -0,0 +1,18 @@
+
+function makeScheduleEvent(title, time, date) {
+    let event = document.createElement('div')
+    .classList.add("event");
+
+    let time = document.createElement('div')
+    .classList.add('eventtime')
+}
+
+
+/*
+
+<div class="event">
+                    <div class="eventtitle">Title showing what this episode is about, and other things</div>
+                    <div class="eventtime">9:00PM - 10:00PM</div>
+                    <div class="eventdate">Friday, September 24</div>
+                </div>
+    */
